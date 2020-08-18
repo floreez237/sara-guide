0d94c941f704a4b0c2d1fa33cd67cd9a4bc5129a
Sevga <sevgama44@gmail.com>
2020-08-18 18:09:15 +0100

    img tag src attribute to change

--- src/components/Split/split.js
+++ src/components/Split/split.js
@@ -1,28 +1,34 @@
 import React,{Component} from 'react';
 import './split_style.scss';
+import img1 from 'http://images.eurogamer.net/2016/metabomb/reinhardttrivial5.jpg';
+import img2 from 'https://images8.alphacoders.com/702/thumb-1920-702883.jpg';
 
-class Split extends  Component {
+export default class Split extends  Component {
+
 
     render(){
         return (
             <div className="container">
-                <section className="screen left">
-                    <h1>SARA Banking</h1>
-                    <button>
-                        <a href="#" className="button">Learn More</a>
-                    </button>
-                </section>
+                <div className="row" id="home">
+                    <div className="column">
+                        <a href="#women" className="men">WOMEN</a> <br/>
+                        <button className="buy"><a href="#womenShop">SHOP</a></button>
+                        <img className="clothing" src="https://i.pinimg.com/originals/49/f7/48/49f7481952681610b7a4a8e1e2aa8e5b.jpg"/>
 
-                <section className="screen right">
-                    <h1>SARA Banking</h1>
-                    <button>
-                        <a href="#" className="button">Learn More</a>
-                    </button>
-                </section>
+                    </div>
+                    <div className="column">
+                        <a href="#men" className="men">MEN</a> <br/>
+                        <button className="buy"><a href="#menShop">SHOP</a></button>
+                        <img src="https://i.pinimg.com/originals/67/19/ac/6719acd0d5901cfac3be51777d2b5171.jpg"/>
+                    </div>
+                    <div className="column">
+                        <a href="#kids" className="men">KIDS</a> <br/>
+                        <button className="buy"><a href="kidsShop">SHOP</a></button>
+                        <img className="clothing" src="https://i.pinimg.com/originals/c6/45/ef/c645ef90429ac3e800f40080476f0151.jpg"/>
+                    </div>
+                </div>
             </div>
         );
     }
 
 }
-
-export default Split;
