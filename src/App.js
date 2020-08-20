7d58956a26358c6ae3482b34d9723df9ce32caa4
floreez237 <florianlowe12@gmail.com>
2020-08-20 08:31:39 +0100

    Completed I18n of the home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
 import MainComponent from './components/MainComponent/MainComponent'
+import {I18nProvider} from "@lingui/react";
+import catalogFr from './locale/fr/messages';
+import catalogEn from './locale/en/messages';
 
 
-function App() {
-  return (
-    <div>
-         <MainComponent/>
-    </div>
-  );
+export default class App extends React.Component {
+    state = {language:"en"}
+
+    changeLanguage = (lang)=>this.setState({language:lang});
+
+    catalogs= {en:catalogEn,fr:catalogFr}
+    render() {
+        return (
+            <I18nProvider language={this.state.language} catalogs={this.catalogs}>
+                <MainComponent onChangeLanguage={this.changeLanguage}/>
+            </I18nProvider>
+        );
+    }
+
+
 }
-
-export default App;
