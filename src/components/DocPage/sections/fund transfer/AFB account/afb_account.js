64ed67623b482eb229908b351f6435bdbdadceac
Sevga <sevgama44@gmail.com>
2020-08-27 16:30:37 +0100

    Service_items modified

--- src/components/DocPage/sections/fund transfer/AFB account/afb_account.js
+++ src/components/DocPage/sections/fund transfer/AFB account/afb_account.js
@@ -1,3 +1,22 @@
+import React from 'react';
+import choose_from_beneficiary_list from './choose_from_beneficiary_list.jpg';
+import enter_beneficiary_account_number from './enter_beneficiary_account_number.jpg';
+import {Trans} from '@lingui/macro';
+
+const AFB_ACCOUNT_ITEMS = [
+    {
+        title:<Trans>Choose From Beneficiary List</Trans>,
+        image:[choose_from_beneficiary_list],
+        description:<Trans>fyjk;glifktfy</Trans>
+    },
+    {
+        title:<Trans>Enter Beneficiary Account Number</Trans>,
+        image:[enter_beneficiary_account_number],
+        description:<Trans>fyjk;glifktfy</Trans>
+    }
+];
+
+export default AFB_ACCOUNT_ITEMS;
 import {Trans} from "@lingui/macro";
 import React from 'react';
 import choose_from_beneficiary_list from './choose_from_beneficiary_list.jpg';
