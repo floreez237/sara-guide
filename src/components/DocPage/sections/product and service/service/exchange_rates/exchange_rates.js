5ccf4f74e6edf4bb7ecd25cfbb0d73f7fdc7aba7
Sevga <sevgama44@gmail.com>
2020-08-27 18:21:45 +0100

    Exchange Rates Completed

---
+++ src/components/DocPage/sections/product and service/service/exchange_rates/exchange_rates.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import exchange_rates from './exchange_rates.jpg';
+
+import {Trans} from '@lingui/macro';
+
+const EXCHANGE_RATES_ITEMS = [
+    {
+        title:<Trans>Exchange Rates</Trans>,
+        image:[exchange_rates],
+        description:<Trans>fyjk;glifktfy</Trans>
+    },
+];
+
+export default EXCHANGE_RATES_ITEMS;
