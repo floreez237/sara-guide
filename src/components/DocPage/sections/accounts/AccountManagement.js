23145e8011e4592d5fda026d971ba3870efebe9b
Ngouoghe Junior <ngouoghejunior@gmail.com>
2020-08-27 15:25:06 +0100

    added images

--- src/components/DocPage/sections/accounts/AccountManagement.js
+++ src/components/DocPage/sections/accounts/AccountManagement.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {Trans} from '@lingui/macro'
 import accountManagement from "./account-management.jpg"
+import add_address from './add-address.jpg'
+import change_PIN from './change-PIN.jpg'
+import user_details from './user-details.jpg'
 
 const ACCOUNT_MANAGEMENT = 
 [
@@ -8,6 +11,21 @@ const ACCOUNT_MANAGEMENT =
         title : <Trans>Account Management</Trans>,
         image : [accountManagement],
         description: <Trans>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis faucibus sollicitudin. Vestibulum arcu mauris, ullamcorper ac neque vel, tempus commodo felis. Nullam at vestibulum nibh, eu maximus tortor. Suspendisse neque justo, blandit ut consectetur at, facilisis ac augue. Etiam tincidunt ullamcorper magna. Nunc dignissim augue vehicula nibh euismod, sit amet facilisis elit suscipit. Nam nunc tortor, tincidunt vel enim ut, porttitor varius magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras elementum sapien tellus, vitae molestie metus elementum fermentum. Etiam non dignissim erat. Sed pulvinar sem ligula, mattis consectetur lacus suscipit a. Mauris hendrerit, lacus non interdum aliquet, orci ipsum faucibus leo, eget ultrices tellus urna a lacus. Sed condimentum ullamcorper metus. Integer quis tincidunt risus, sit amet pulvinar purus. In rhoncus felis ac velit faucibus ultrices. Duis metus nunc, vehicula eget tortor nec, sagittis ultrices metus.</Trans>
+    },
+    {
+        title : <Trans>Edit user Details</Trans>,
+        image : [user_details],
+        description: <Trans>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis faucibus sollicitudin. Vestibulum arcu mauris, ullamcorper ac neque vel, tempus commodo felis. Nullam at vestibulum nibh, eu maximus tortor. Suspendisse neque justo, blandit ut consectetur at, facilisis ac augue. Etiam tincidunt ullamcorper magna. Nunc dignissim augue vehicula nibh euismod, sit amet facilisis elit suscipit. Nam nunc tortor, tincidunt vel enim ut, porttitor varius magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras elementum sapien tellus, vitae molestie metus elementum fermentum. Etiam non dignissim erat. Sed pulvinar sem ligula, mattis consectetur lacus suscipit a. Mauris hendrerit, lacus non interdum aliquet, orci ipsum faucibus leo, eget ultrices tellus urna a lacus. Sed condimentum ullamcorper metus. Integer quis tincidunt risus, sit amet pulvinar purus. In rhoncus felis ac velit faucibus ultrices. Duis metus nunc, vehicula eget tortor nec, sagittis ultrices metus.</Trans> 
+    },
+    {
+        title : <Trans>Change PIN</Trans>,
+        image : [change_PIN],
+        description : <Trans>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis faucibus sollicitudin. Vestibulum arcu mauris, ullamcorper ac neque vel, tempus commodo felis. Nullam at vestibulum nibh, eu maximus tortor. Suspendisse neque justo, blandit ut consectetur at, facilisis ac augue. Etiam tincidunt ullamcorper magna. Nunc dignissim augue vehicula nibh euismod, sit amet facilisis elit suscipit. Nam nunc tortor, tincidunt vel enim ut, porttitor varius magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras elementum sapien tellus, vitae molestie metus elementum fermentum. Etiam non dignissim erat. Sed pulvinar sem ligula, mattis consectetur lacus suscipit a. Mauris hendrerit, lacus non interdum aliquet, orci ipsum faucibus leo, eget ultrices tellus urna a lacus. Sed condimentum ullamcorper metus. Integer quis tincidunt risus, sit amet pulvinar purus. In rhoncus felis ac velit faucibus ultrices. Duis metus nunc, vehicula eget tortor nec, sagittis ultrices metus.</Trans> 
+    },
+    {
+        title : <Trans>Add Address</Trans>,
+        image : [add_address],
+        description : <Trans>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis faucibus sollicitudin. Vestibulum arcu mauris, ullamcorper ac neque vel, tempus commodo felis. Nullam at vestibulum nibh, eu maximus tortor. Suspendisse neque justo, blandit ut consectetur at, facilisis ac augue. Etiam tincidunt ullamcorper magna. Nunc dignissim augue vehicula nibh euismod, sit amet facilisis elit suscipit. Nam nunc tortor, tincidunt vel enim ut, porttitor varius magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras elementum sapien tellus, vitae molestie metus elementum fermentum. Etiam non dignissim erat. Sed pulvinar sem ligula, mattis consectetur lacus suscipit a. Mauris hendrerit, lacus non interdum aliquet, orci ipsum faucibus leo, eget ultrices tellus urna a lacus. Sed condimentum ullamcorper metus. Integer quis tincidunt risus, sit amet pulvinar purus. In rhoncus felis ac velit faucibus ultrices. Duis metus nunc, vehicula eget tortor nec, sagittis ultrices metus.</Trans> 
     }
 ]
 
