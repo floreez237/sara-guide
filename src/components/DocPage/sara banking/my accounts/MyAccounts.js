7aed6b3cd3ec4890bdb418129d23eb6b7cbc76a0
floreez237 <florianlowe12@gmail.com>
2020-09-08 15:43:07 +0100

    sara banking structure

---
+++ src/components/DocPage/sara banking/my accounts/MyAccounts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Trans} from '@lingui/macro';
+import accountList from './My Accounts.jpg';
+import home from './../home.jpg';
+
+const MY_ACCOUNTS =[
+    {
+        title:<Trans>Accounts List</Trans>,
+        image:[home, accountList],
+        description:<Trans>dsdsd</Trans>
+    }
+]
